5d5180b28b18f98e69e0d92b8b8d50be56e6204a
Glauco Oliveira <glaucodiogoalves@gmail.com>
2021-04-04 16:49:35 -0300

    Criando dominios  1 => N

---
+++ Curso/Domain/Departamento.cs
@@ -0,0 +1,11 @@
+namespace Curso.Domain
+{
+    public class Departamento
+    {
+        public int Id {get;set;}
+        public string Descricao {get;set;}
+        public bool Ativo {get;set;}
+
+        public List<Funcionario> Funcionarios {get;set;}
+    }
+}
---
+++ Curso/Domain/Funcionario.cs
@@ -0,0 +1,10 @@
+namespace Curso.Domain{
+    public class Funcionario{
+        public int Id {get;set;}
+        public string Nome {get;set;}
+        public string Cpf {get;set;}
+
+        public int DepartamentoId {get;set;}
+        public Departamento Departamento {get;set;}
+    }
+}
